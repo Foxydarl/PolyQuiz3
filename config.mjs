62ba902caba9184be7958e69aa665ddab3c94b5f
Foxy Darl <boris.panna@gmail.com>
2025-03-21 15:18:41 +0500

    test9

--- config.mjs
+++ config.mjs
@@ -1,4 +1,4 @@
-export const WEBSOCKET_PUBLIC_URL = "ws://localhost:5505"
+export const WEBSOCKET_PUBLIC_URL = "ws://polyquiz.aspc.kz:3000/"
 export const WEBSOCKET_SERVER_PORT = 5505
 
 const QUIZZ_CONFIG = {
